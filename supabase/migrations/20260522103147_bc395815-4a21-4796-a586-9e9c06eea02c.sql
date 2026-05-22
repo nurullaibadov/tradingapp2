822d9ed6f5375d764a201f89e6886d8235175d8d
Nurulla Ibadov <151769024+nurullaibadov@users.noreply.github.com>
2026-05-22 18:05:24 +0400

    Delete supabase directory

--- supabase/migrations/20260522103147_bc395815-4a21-4796-a586-9e9c06eea02c.sql
+++
@@ -1,4 +0,0 @@
-
-REVOKE EXECUTE ON FUNCTION public.has_role(UUID, app_role) FROM PUBLIC, anon;
-GRANT EXECUTE ON FUNCTION public.has_role(UUID, app_role) TO authenticated;
-REVOKE EXECUTE ON FUNCTION public.handle_new_user() FROM PUBLIC, anon, authenticated;
